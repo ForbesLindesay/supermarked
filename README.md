2fd8af106db067f947d1d0d875833d6701044764
ForbesLindesay <forbes@lindesay.co.uk>
2013-01-05 04:09:34 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-supermarked
-===========
+# supermarked
 
-marked with syntax highlighting and ascii-math by default
+  Marked with syntax highlighting and ascii-math by default
+
+## API
+
+```javascript
+var marked = require('supermarked');
+marked('markdown string', options);
+```
+
+## Extensions currently supported
+
+### Syntax highlighting
+
+  Syntax highlighting using highlight.js is enabled by default for code samples (requires css to actually have an impact)
+
+### ascii-math
+
+  You can create math in two ways, it can go inline in a paragraph like $e^(i pi)=-1$ or it can be a block
+
+    $e^(i pi)=-1$
+
+  To have it like a block, simply indent it 4 spaces like code and surround with $ signs.
+
+### Mentions
+
+  You can use `service: @user` anywhere to mention someone and it will turn into a social link.  Supported services are `twitter`, `github`, `npm`, and `facebook`.  All of them are case sensetive and the space between `:` and `@` is optional.
+
+  In addition to this there is `local: @user` which becomes a link to `/user/:user:` (where `:user:` is replaced with the username of the person being mentioned).  This is also aliased as the default if you just type `@user`
---
+++ index.js
@@ -0,0 +1,76 @@
+var marked = require('marked');
+var hljs = require('highlight.js');
+var math = require('ascii-math');
+
+var exports = module.exports = supermarked;
+function supermarked(src, options) {
+  options = options || {};
+  options.gfm = options.gfm !== false;
+  var aliases = options.aliases || exports.aliases;
+  options.highlight = options.highlight || function (code, lang) {
+    if (lang) {
+      try {
+        return hljs.highlight(aliases[lang.toLowerCase()] || lang.toLowerCase(), code).value;
+      } catch (ex) {} //let marked automatically escape code in a language we don't speak
+    } else {
+      try {
+        if (!options.ignoreMath && /^\$.+\$$/.test(code))
+          var res = math(code.substring(1, code.length - 1))
+          res.setAttribute('style', 'display: block;')
+          return res.toString();
+      } catch (ex) {}
+    }
+  }
+  var tokens = marked.lexer(src, options);
+  var result = marked.parser(tokens, options)
+  if (!options.ignoreMentions) {
+    var services = options.services || exports.services;
+    result = result.replace(/([^ ]+): *@([A-Za-z0-9_-]+)/, function (_, service, user) {
+        service = service.toLowerCase();
+        if (services[service]) {
+          return '<a href="'
+           + services[service].replace(':user:', user)
+           + '" class="user-profile user-profile-'
+           + service 
+           + '">@' 
+           + user 
+           + '</a>';
+        } else {
+          return _;
+        }
+      })
+      .replace(/ @([A-Za-z0-9_-]+)/, function (_, user) {
+        if (services['@']) {
+          return ' <a href="'
+           + services['@'].replace(':user:', user)
+           + '" class="user-profile">@' 
+           + user 
+           + '</a>';
+        } else {
+          return _;
+        }
+      });;
+  }
+  if (!options.ignoreMath) {
+    result = result.replace(/<pre><code><math/g, '<math').replace(/<\/math><\/code><\/pre>/g, '</math>')
+      .replace(/\\\$/g, '__DOLLAR_SIGN__')
+      .replace(/\$(.*)\$/g, function (_, equation) {
+        return math(equation.replace(/\\\$/g, '$')).toString();
+      })
+      .replace(/__DOLLAR_SIGN__/g, '$');
+  }
+  return result;
+}
+
+var services = exports.services = {
+  'twitter': 'https://twitter.com/:user:',
+  'github': 'https://github.com/:user:',
+  'npm': 'https://npmjs.org/~:user:',
+  'facebook': 'https://www.facebook.com/:user:',
+  'local': '/user/:user:'
+};
+services['@'] = services.local;//set the default here
+
+var alises = exports.aliases = {
+  'js': 'javascript'
+};
